23bf1c2df625a52c2036d4fb20f8bfe5ce74b823
guadorsa <ortizgustavo8121@gmail.com>
2024-05-02 13:57:14 -0500

    se reaizan adiciones de getters y setters, se crea clase estudiante heredadad de la clase persona

--- ejercicio 2/main.js
+++ ejercicio 2/main.js
@@ -1,13 +1,51 @@
 class Persona{
-    constructor(nombre, edad){
+    constructor(nombre, apellido, edad, sexo, peso, altura){
         this.nombre=nombre;
+        this.apellido=apellido;
         this.edad=edad;
+        this.sexo=sexo;
+        this.peso=peso;
+        this.altura=altura;
     }
+
+    getNombre(){
+        return this.nombre;
+    }
+
+    setNombre(nuevoNombre){
+        this.nombre = nuevoNombre;
+    }
+
+    getEdad(){
+        return this.edad;
+    }
+
+    setEdad(nuevoEdad){
+        this.edad = nuevoEdad;
+    }  
+
     Saludar(){
         console.log(`hola soy ${this.nombre} y tengo ${this.edad} años`);
         }
 }
+class Estudiante extends Persona{
+    constructor(nombre, apellido, edad, carrera){
+        super(nombre, apellido, edad);
+        this.carrera=carrera;
+    }
+}
+
 let persona1 = new Persona("Juan", 30);
 persona1.Saludar();
+persona1.getEdad;
+persona1.setEdad;
+persona1.getNombre;
+persona1.setNombre;
 console.log(persona1.nombre);
 console.log(persona1.edad);
+
+const estudiante1=new Estudiante("juan","ortiz", 34, "sistemas");
+console.log(estudiante1.nombre);
+console.log(estudiante1.carrera);
+estudiante1.Saludar();
+
