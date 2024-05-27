e7400679ff12c8020adf1b3dfe655e4282ab4c5c
guadorsa <ortizgustavo8121@gmail.com>
2024-05-27 12:12:38 -0500

    ok

--- ejercicio 2/main.js
+++ ejercicio 2/main.js
@@ -60,5 +60,7 @@ const estudiante1=new Estudiante("juan","ortiz", 34, "sistemas");
 console.log(estudiante1.nombre);
 console.log(estudiante1.carrera);
 estudiante1.Saludar();
-console.log (persona1._identificacion)
+console.log (persona1._identificacion);
+console.log (persona1._obtenerIdentificacion(5,5));
+
 
